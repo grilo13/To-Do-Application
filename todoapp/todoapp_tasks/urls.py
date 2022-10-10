e3aab4f5b1eca84ecda78fac15162cc25e25ffd3
pedro.grilo <pedro.sgrilo@decsis.pt>
2022-10-10 10:50:57 +0100

    Login and register page created and designed, tasks implement but no design

--- todoapp/todoapp_tasks/urls.py
+++ todoapp/todoapp_tasks/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import ListTasksView, GetTaskView, DeleteTaskView, TaskReorderView
+from .views import ListTasksView, GetTaskView, DeleteTaskView, TaskReorderView, CreateTask, CompleteTask
 
 urlpatterns = [
     path('', ListTasksView.as_view(), name='list_of_tasks'),
+    path('create/', CreateTask.as_view(), name='create_task'),
     path('<int:id>/', GetTaskView.as_view(), name='get_task_by_id'),
     path('delete/<int:id>/', DeleteTaskView.as_view(), name='delete_task_by_id'),
     path('reorder', TaskReorderView.as_view(), name='reorder_list_of_tasks'),
+    path('complete/<int:id>/', CompleteTask.as_view(), name='complete_task'),
 ]
